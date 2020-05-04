b263d5fc2f689dbeecd4c7959f6bb824dee97fd3
gboyega.a@credpal.com <gboyega.a@credpal.com>
2020-05-04 16:17:21 +0100

    created a spider that scrapes the btc tab in the livecoin.com/en website

--- livecoin/spiders/coin.py
+++ livecoin/spiders/coin.py
@@ -2,21 +2,23 @@
 import scrapy
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from scrapy.selector import Selector
 
 class CoinSpider(scrapy.Spider):
     name = 'coin'
     allowed_domains = ['www.livecoin.net/en']
-    # start_urls = ['http://www.livecoin.net/en']
+    start_urls = ['http://www.livecoin.net/en']
 
     def __init__(self):
         chrome_options = Options()
         chrome_options.add_argument("--headless")
 
-        driver = webdriver.Chrome(executable_path="./chromedriver")
+        driver = webdriver.Chrome(executable_path="./chromedriver", options=chrome_options)
+        driver.set_window_size(1920, 1080)
         driver.get('http://www.livecoin.net/en')
 
         rur_tab = driver.find_elements_by_class_name("filterPanelItem___2z5Gb")
-        rur_tab[4]
+        rur_tab[0].click()
 
         self.html = driver.page_source
         driver.close()
@@ -24,4 +26,10 @@ class CoinSpider(scrapy.Spider):
 
 
     def parse(self, response):
-        pass
+        resp = Selector(text=self.html)
+
+        for currency in resp.xpath("//div[contains(@class, 'ReactVirtualized__Table__row tableRow___3EtiS ')]"):
+            yield {
+                'currency': currency.xpath(".//div[1]/div/text()").get(),
+                'volumn(24hr)': currency.xpath(".//div[2]/span/text()").get()
+            }
